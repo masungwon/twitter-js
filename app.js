e70a0a083408755bc54381d53ba4f4e1141c1588
Sungwon Ma <sma2@swarthmore.edu>
2016-08-02 15:41:01 -0400

    adding nodemon

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ app.listen(3000, function() {
 	console.log("server listening");
 });
 
+app.use('/', function(req, res, next) {
+	//need to get verb & url
+	console.log(req.method, req.url, res.statusCode);
+	next();
+})
+
+app.all('/news', function(req, res, next) {
+	console.log("you've reached the news area");
+	next();
+})
+
 app.get('/', function(req, res) {
 	res.send("hello! this is the callback");
+});
+
+app.get('/news', function(req, res) {
+	res.send("no news is good news!");
 });
