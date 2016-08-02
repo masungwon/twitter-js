2e9a797583dcb20757fc7618205efaed3685c9dc
Sungwon Ma <sma2@swarthmore.edu>
2016-08-02 17:42:42 -0400

    making router file and tweetBank

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
 var express = require('express');
 var app = express();
+var swig = require('swig');
+var routes = require('./routes/');
+
+app.use('/', routes);
+
+
+
+swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
+    console.log(output);
+});
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+
+app.set('views', __dirname + '/views');
+
+swig.setDefaults({ cache: false });
 
 app.listen(3000, function() {
 	console.log("server listening");
 });
 
-app.use('/', function(req, res, next) {
+app.all('/', function(req, res, next) {
 	//need to get verb & url
 	console.log(req.method, req.url, res.statusCode);
+	res.send("under construction");
 	next();
 })
 
-app.all('/news', function(req, res, next) {
-	console.log("you've reached the news area");
-	next();
-})
+app.get('/tweets', function(req, res) {
+	//res.send("hello! this is the callback");
+	// var people = [{name: 'Ingrid'}, {name: 'Sungwon'}, {name: 'Emily'}];
+	// res.render( 'index', {title: 'Grace Hopper People', people: people} );
 
-app.get('/', function(req, res) {
-	res.send("hello! this is the callback");
+
 });
 
-app.get('/news', function(req, res) {
-	res.send("no news is good news!");
+app.post('/tweets', function(req, res) {
+	//res.send("no news is good news!");
 });
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+var tweetBank = require('../tweetBank');
+
+router.get('/', function (req, res) {
+  var tweets = tweetBank.list();
+  res.render( 'index', { tweets: tweets } );
+});
+
+module.exports = router;
+
+router.get('/stylesheets/style.css', function (req, res) {
+	res.sendFile(path.join(__dirname, '../public/stylesheets/style.css'));
+
+	//why doesn't this work?
+	//res.sendFile(__dirname + '../public/stylesheets/style.css');
+})
+
+
+
