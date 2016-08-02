bc42a3c9f794b7280b6c174660d8a1b8321fa319
Sungwon Ma <sma2@swarthmore.edu>
2016-08-02 18:33:18 -0400

    making progress with showing user's tweets

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,17 @@ module.exports = router;
 
 router.use(express.static('public'));
 
+// say that a client GET requests the path /users/nimit
+router.get( '/users/:name', function (req, res) {
+  var tweets = tweetBank.find(req.params.name, function(item) {
+  	return item === req.params.name;
+  });
+	res.render( 'index', { tweets: tweets } );
+	console.log(req.params.name, tweets);
+
+   // --> 'nimit'
+});
+
 // router.get('/stylesheets/style.css', function (req, res) {
 // 	res.sendFile(path.join(__dirname, '../public/stylesheets/style.css'));
 
